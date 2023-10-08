09101cc63c7e68ce8cd551b285c7b56abdbb03a0
ozgeturkdag <ozgeturkdag@gmail.com>
2023-10-08 15:04:27 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const options = {
+  method: "GET",
+  url: "https://text-translator2.p.rapidapi.com/getLanguages",
+  headers: {
+    "X-RapidAPI-Key": "5332c479c4msh4dc29c530a8fb43p19df6ejsnd1b202c1f5ab",
+    "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+  },
+};
